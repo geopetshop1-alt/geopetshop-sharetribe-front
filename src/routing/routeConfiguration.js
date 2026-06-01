8268558b135c920e5f0ffb3fd5d0cc7a6657e3ec
geopetshop1-alt <geopetshop1@gmail.com>
2026-06-01 15:31:12 -0300

    Update routeConfiguration.js

--- src/routing/routeConfiguration.js
+++ src/routing/routeConfiguration.js
@@ -21,6 +21,7 @@ const EmailVerificationPage = loadable(() => import(/* webpackChunkName: "EmailV
 const InboxPage = loadable(() => import(/* webpackChunkName: "InboxPage" */ '../containers/InboxPage/InboxPage'));
 const MakeOfferPage = loadable(() => import(/* webpackChunkName: "MakeOfferPage" */ '../containers/MakeOfferPage/MakeOfferPage'));
 const LandingPage = loadable(() => import(/* webpackChunkName: "LandingPage" */ '../containers/LandingPage/LandingPage'));
+const LeadFormPage = loadable(() => import(/* webpackChunkName: "LeadFormPage" */ '../containers/LeadFormPage/LeadFormPage'));
 const ListingPageCoverPhoto = loadable(() => import(/* webpackChunkName: "ListingPageCoverPhoto" */ /* webpackPrefetch: true */ '../containers/ListingPage/ListingPageCoverPhoto'));
 const ListingPageCarousel = loadable(() => import(/* webpackChunkName: "ListingPageCarousel" */ /* webpackPrefetch: true */ '../containers/ListingPage/ListingPageCarousel'));
 const ManageListingsPage = loadable(() => import(/* webpackChunkName: "ManageListingsPage" */ '../containers/ManageListingsPage/ManageListingsPage'));
@@ -81,6 +82,11 @@ const routeConfiguration = (layoutConfig, accessControlConfig) => {
       name: 'LandingPage',
       component: LandingPage,
       loadData: pageDataLoadingAPI.LandingPage.loadData,
+    },
+    {
+      path: '/formulario',
+      name: 'LeadFormPage',
+      component: LeadFormPage,
     },
     {
       path: '/p/:pageId',
