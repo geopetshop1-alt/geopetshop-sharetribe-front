93c7cb31eb31f4571e2ac659f08c8ef23b203c38
geopetshop1-alt <geopetshop1@gmail.com>
2026-06-05 12:23:55 -0300

    Update LandingPage.duck.js

--- src/containers/LandingPage/LandingPage.duck.js
+++ src/containers/LandingPage/LandingPage.duck.js
@@ -1,5 +1,5 @@
 import { fetchPageAssets } from '../../ducks/hostedAssets.duck';
-export const ASSET_NAME = 'landing-page';
+export const ASSET_NAME = 'landing-page-v3';
 
 export const loadData = (params, search) => dispatch => {
   const pageAsset = { landingPage: `content/pages/${ASSET_NAME}.json` };
