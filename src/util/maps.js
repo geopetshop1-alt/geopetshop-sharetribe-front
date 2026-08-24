59679e62124156be74639dac1526b44e9d08bae1
geopetshop1-alt <geopetshop1@gmail.com>
2026-08-24 12:53:34 -0300

    GPS-15: increase geolocation timeout on mobile

--- src/util/maps.js
+++ src/util/maps.js
@@ -110,7 +110,7 @@ export const userLocation = () =>
     //       We decided that true would be better default for a template app.
     const options = {
       enableHighAccuracy: true,
-      timeout: 5000,
+      timeout: 15000,
       maximumAge: 0,
     };
 
